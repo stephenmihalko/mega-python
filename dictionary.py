d14c6458dfaf42c3f09f6fc853efb29f2e6bd00e
Stephen Mihalko <stephen.mihalko@gmail.com>
2019-02-19 17:58:18 -0500

    Fixed no match found indexing error

--- dictionary.py
+++ dictionary.py
@@ -3,11 +3,15 @@ import difflib	# for determining differences in strings
 
 def definition(word):
 	word = word.lower()
+	# This is the most likely result and the result that requires the least actual work!
 	if word in data:
 		return data[word]
 	else:
-		potential = difflib.get_close_matches(word, data.keys(), n = 1)
-		return ("The word does not exist. Did you mean %s?" % potential[0])
+		potentials = difflib.get_close_matches(word, data.keys())
+		if len(potentials) > 0:
+			return ("The word does not exist. Did you mean '%s'?" % potentials[0])
+		else:
+			return "The word does not exist. Please try again."
 
 # This is a dictionary where words are keys and definitions are values.
 data = json.load(open("data.json"))
