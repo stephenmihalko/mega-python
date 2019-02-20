9c3c74b79b56140e0d0fa247421a0862a55eda80
stephenmihalko <47727340+stephenmihalko@users.noreply.github.com>
2019-02-20 13:01:10 -0500

    Change zoom_level to zoom_start

--- webmap.py
+++ webmap.py
@@ -5,6 +5,6 @@ import folium
 
 # Create the basemap layer
 the_geographic_center = [39.8285354,-98.579482]	# LAT north, LONG WEST
-# TODO: add zoom_level to Map constructor
+# TODO: add zoom_start to Map constructor
 basemap = folium.Map(location=the_geographic_center)
 basemap.save("basemap.html")
