99a5020f8d7be5d8e6a505963b4cdeaf4c63bdeb
stephenmihalko <47727340+stephenmihalko@users.noreply.github.com>
2019-02-20 12:56:20 -0500

    Add creating basemap layer and export to html

--- webmap.py
+++ webmap.py
@@ -1,1 +1,9 @@
 # TODO: everything
+# 1. pip3 install folium
+
+import folium
+
+# Create the basemap layer
+the_geographic_center = [39.8285354,-98.579482]	# LAT north, LONG WEST
+basemap = folium.Map(location=the_geographic_center)
+basemap.save("basemap.html")
