24e42570a27f1d5b6ed0c334faf2e335ddce8267
stephenmihalko <47727340+stephenmihalko@users.noreply.github.com>
2019-02-20 16:05:35 -0500

    Added reading information from text file

--- webmap.py
+++ webmap.py
@@ -1,7 +1,10 @@
 # TODO: everything
 # 1. pip3 install folium
 
+import pandas
 import folium
+
+from_file = True
 
 # Create the basemap layer
 geographic_center = [39.8285354,-98.579482]	# LAT north, LONG WEST
@@ -9,7 +12,16 @@ basemap = folium.Map(location=geographic_center, zoom_start=4, tiles="Mapbox Bri
 
 # Create a feature group to add markers.
 fg = folium.FeatureGroup(name="My Map")
-fg.add_child(folium.Marker(location=[39, -99], popup="Marker message!", icon=folium.Icon(color="blue")))
+
+if from_file:
+	data = pandas.read_csv("Volcanoes.txt")
+	lats = list(data["LAT"])
+	lons = list(data["LON"])
+	names = list(data["NAME"])
+	elevs = list(data["ELEV"])
+
+for lt, ln, nm, el in zip(lats, lons, names, elevs):
+	fg.add_child(folium.Marker(location=[lt, ln], popup=("%s is %s m high" % (nm, el)), icon=folium.Icon(color="blue")))
 
 basemap.add_child(fg)
 
