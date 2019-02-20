2bc643d243775f13e9a7a41a5eb9e23bc816adb6
stephenmihalko <47727340+stephenmihalko@users.noreply.github.com>
2019-02-20 15:42:11 -0500

    Add FeatureGroup object to store markers

--- webmap.py
+++ webmap.py
@@ -6,4 +6,11 @@ import folium
 # Create the basemap layer
 geographic_center = [39.8285354,-98.579482]	# LAT north, LONG WEST
 basemap = folium.Map(location=geographic_center, zoom_start=4, tiles="Mapbox Bright")
+
+# Create a feature group to add markers.
+fg = folium.FeatureGroup(name="My Map")
+fg.add_child(folium.Marker(location=[39, -99], popup="Marker message!", icon=folium.Icon(color="blue")))
+
+basemap.add_child(fg)
+
 basemap.save("basemap.html")
