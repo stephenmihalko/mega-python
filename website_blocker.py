cacab8261e2530a8b34a4c9d35121cd6c19425e1
stephenmihalko <47727340+stephenmihalko@users.noreply.github.com>
2019-02-21 17:28:24 -0500

    Add comments and test data

--- website_blocker.py
+++ website_blocker.py
@@ -2,16 +2,24 @@ import platform
 import time
 from datetime import datetime
 
-hosts_path = r"C:\Windows\System32\drivers\etc\hosts" if platform.system() == "Windows" else "/etc/hosts"
+hosts_path = "hosts"
+#hosts_path = r"C:\Windows\System32\drivers\etc\hosts" if platform.system() == "Windows" else "/etc/hosts"
 redirect = "127.0.0.1"
 sites = ["www.facebook.com", "mail.google.com"]
 
 while True:
 	with open(hosts_path, "r+") as hosts:
-		if datetime.now().weekday() < 5 and 8 <= datetime.now().hour < 16:
+		# If Monday-Friday (0-5) and between 8a and 4p
+		if datetime.now().weekday() < 5 and 8 <= datetime.now().hour < 20:
 			content = hosts.read()
+			# Check to see if each site is in the content.
 			for site in sites:
+				# This is "continue" in other languages.
 				if site in content:
 					pass
+				# Put the site in the file.
 				else:
-					file.write(redirect + " " + site + "\n")
+					hosts.write(redirect + " " + site + "\n")
+
+	# Wait a minute so as not to tax resources.
+	time.sleep(60)
