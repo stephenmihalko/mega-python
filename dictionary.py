4e9a9d7d62f8d84cd70906c6bed0616974ea3032
stephenmihalko <47727340+stephenmihalko@users.noreply.github.com>
2019-02-21 11:09:26 -0500

    Added all caps and title case functionality

--- dictionary.py
+++ dictionary.py
@@ -2,16 +2,23 @@ import json	# for reading json text files
 import difflib	# for determining differences in strings
 
 def definition(word):
-	word = word.lower()
 	# This is the most likely result and the result that requires the least actual work!
-	if word in data:
-		# There's definitely a more pythonic way to do this...
-		str = ""
-		for el in data[word]:
-			str += ("- %s\n" % el)
-		return str[:-1];
+	if word.lower() in data:
+		# This joins the definitions with the newline character - the more pythonic way!
+		return "\n".join(data[word.lower()])
+	
+	# Maybe they're looking for something like "USA"
+	elif word.upper() in data:
+		return "\n".join(data[word.upper()])
+	
+	# Maybe they're looking for a proper noun!
+	elif word.capitalize() in data:
+		return "\n".join(data[word.capitalize()])
+	
+	# Maybe they misspelled it.
 	else:
-		potentials = difflib.get_close_matches(word, data.keys())
+		# Getting the four best matches
+		potentials = difflib.get_close_matches(word.lower(), data.keys(), n=4)
 		if len(potentials) > 0:
 			return data[potentials[0]] if input("The word does not exist. Did you mean '%s'? Enter Y or N: " % potentials[0]) == "Y" else "Sorry about that."
 		else:
