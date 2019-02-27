4598c63f9642b95c9e5071abc119ffc0aa8f148f
Stephen Mihalko <stephen.mihalko@gmail.com>
2019-02-26 20:08:53 -0500

    Start window and regex

---
+++ tkinter-gui/dumb_conversions.py
@@ -0,0 +1,12 @@
+from tkinter import *
+import re
+
+# I'm putting the callback function up here. Come and stop me!
+def convert():
+	
+
+# The "is this only numbers" pattern
+num_patt = re.compile(r"^\d+$")
+
+# Create a new window
+window = Tk()
