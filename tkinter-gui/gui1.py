dd9b6c60bf6b829bca17baea5c86ed86c451581e
Stephen Mihalko <stephen.mihalko@gmail.com>
2019-02-26 19:29:45 -0500

    Create window and button

--- tkinter-gui/gui1.py
+++ tkinter-gui/gui1.py
@@ -1,1 +1,14 @@
-import tkinter
+# This is better than import tkinter because it puts us in the tkinter namespace.
+from tkinter import *
+
+# Create an empty window.
+window = Tk()
+
+# Create a button and tell it what window to go to
+b1 = Button(window, text="Execute")
+# Put the button on the window
+b1.pack()
+
+
+# This is to keep the window going - otherwise it exits manually.
+window.mainloop()
