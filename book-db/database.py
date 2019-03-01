067bdd78cde94f6b66819bb90e035cfad2988d72
Stephen Mihalko <stephen.mihalko@gmail.com>
2019-02-28 19:18:41 -0500

    Add labels

--- book-db/database.py
+++ book-db/database.py
@@ -1,6 +1,12 @@
 from tkinter import *
 
+gui = Tk()
+
 # Static labels
+Label(gui, text="Title").grid(row=0, column=0)
+Label(gui, text="Author").grid(row=0, column=2)
+Label(gui, text="Year").grid(row=2, column=0)
+Label(gui, text="ISBN").grid(row=2, column=2)
 
 # Entry boxes
 
@@ -9,3 +15,6 @@ from tkinter import *
 # Scrollbar
 
 # Buttons
+
+
+gui.mainloop()
