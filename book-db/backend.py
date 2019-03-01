25df11f122943a1b7f486f42c5500c5a2929af12
stephenmihalko <47727340+stephenmihalko@users.noreply.github.com>
2019-03-01 13:47:03 -0500

    Write view_all() function

--- book-db/backend.py
+++ book-db/backend.py
@@ -1,11 +1,24 @@
+import sqlite3
 # id, title, author, year, isbn
 
 # View all
-
+def view_all():
+    conn = sqlite3.connect("books_db.db")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM Books")
+    rows = cur.fetchall()
+    conn.close()
+    return rows
+    
 # Search for record
-
+def search():
+    
 # Add record
-
+def add():
+    
 # Update record
-
+def update():
+    
 # Delete record
+def delete():
+    
