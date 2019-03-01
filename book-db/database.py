442d655c70250e2ea40c37c93e0b3bb0f7baaab0
Stephen Mihalko <stephen.mihalko@gmail.com>
2019-02-28 19:22:10 -0500

    Add entry boxes

--- book-db/database.py
+++ book-db/database.py
@@ -9,6 +9,17 @@ Label(gui, text="Year").grid(row=2, column=0)
 Label(gui, text="ISBN").grid(row=2, column=2)
 
 # Entry boxes
+title_e = Entry(gui)
+title_e.grid(row=0, column=1)
+
+author_e = Entry(gui)
+author_e.grid(row=0, column=3)
+
+year_e = Entry(gui)
+year_e.grid(row=2, column=1)
+
+isbn_e = Entry(gui)
+isbn_e.grid(row=2, column=3)
 
 # Listbox
 
