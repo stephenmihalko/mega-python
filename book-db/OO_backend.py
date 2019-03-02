0459b86cec89c74ccae0b94bc048be14249e5028
stephenmihalko <47727340+stephenmihalko@users.noreply.github.com>
2019-03-01 19:03:56 -0500

    Change script to OO program
    Backend fully reconstructed
    Moving initial frontend setup to constructor

--- book-db/OO_backend.py
+++ book-db/OO_backend.py
@@ -1,56 +1,51 @@
 import sqlite3
 # id, title, author, year, isbn
 
-# Connect and create the table
-def connect():
-    conn = sqlite3.connect("books_db.db")
-    cur = conn.cursor()
-    cur.execute("CREATE TABLE IF NOT EXISTS Books (id INTEGER PRIMARY KEY, title VARCHAR(100) NOT NULL, author VARCHAR(50) NOT NULL, year INTEGER, isbn VARCHAR(17) NOT NULL)")
-    conn.commit()
-    conn.close()
+class BooksBackend:
 
-# View all
-def view_all():
-    conn = sqlite3.connect("books_db.db")
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM Books")
-    rows = cur.fetchall()
-    conn.close()
-    return rows
+    # Connect and create the table in CONSTRUCTOR
+    def __init__(self):
     
-# Search for record
-def search(title="", author="", year="", isbn=""):
-    conn = sqlite3.connect("books_db.db")
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM Books WHERE title=? OR author=? OR year=? OR isbn=?", (title, author, year, isbn))
-    rows = cur.fetchall()
-    conn.close()
-    return rows
-    
-# Add record
-def insert(title, author, year, isbn):
-    conn = sqlite3.connect("books_db.db")
-    cur = conn.cursor()
-    cur.execute("INSERT INTO Books VALUES (NULL, ?, ?, ?, ?)", (title, author, year, isbn))
-    conn.commit()
-    conn.close()
-    
-# Update record
-def update(id, title, author, year, isbn):
-    conn = sqlite3.connect("books_db.db")
-    cur = conn.cursor()
-    cur.execute("UPDATE Books SET title=?, author=?, year=?, isbn=? WHERE id = ?", (title, author, year, isbn, id))
-    conn.commit()
-    conn.close()
-    
-# Delete record
-def delete(id):
-    conn = sqlite3.connect("books_db.db")
-    cur = conn.cursor()
-    # We can get away with using only ID because the UI requires the user to select a book from the list and this has the ID.
-    cur.execute("DELETE FROM Books WHERE id = ?", (id,))
-    conn.commit()
-    conn.close()
+        # ---- INSTANCE VARIABLES ----
+        self.conn = sqlite3.connect("books_db.db")
+        self.cur = self.conn.cursor()
 
+        self.cur.execute("CREATE TABLE IF NOT EXISTS Books (id INTEGER PRIMARY KEY, title VARCHAR(100) NOT NULL, author VARCHAR(50) NOT NULL, year INTEGER, isbn VARCHAR(17) NOT NULL)")
+        commit()
 
-connect()
+    # View all
+    def view_all(self):
+        self.cur.execute("SELECT * FROM Books")
+        rows = self.cur.fetchall()
+        return rows
+    
+    # Search for record
+    def search(self, title="", author="", year="", isbn=""):
+        self.cur.execute("SELECT * FROM Books WHERE title=? OR author=? OR year=? OR isbn=?", (title, author, year, isbn))
+        rows = self.cur.fetchall()
+        return rows
+    
+    # Add record
+    def insert(self, title, author, year, isbn):
+        self.cur.execute("INSERT INTO Books VALUES (NULL, ?, ?, ?, ?)", (title, author, year, isbn))
+        commit()
+        
+    # Update record
+    def update(self, id, title, author, year, isbn):
+        self.cur.execute("UPDATE Books SET title=?, author=?, year=?, isbn=? WHERE id = ?", (title, author, year, isbn, id))
+        commit()
+        
+    # Delete record
+    def delete(self, id):
+        # We can get away with using only ID because the UI requires the user to select a book from the list and this has the ID.
+        self.cur.execute("DELETE FROM Books WHERE id = ?", (id,))
+        commit()
+
+    # Commit changes
+    def commit(self):
+        self.conn.commit()
+
+    # Close the connection to the database before we close.
+    def __del__(self):
+        self.conn.close()
+    
--- book-db/OO_frontend.py
+++ book-db/OO_frontend.py
@@ -1,101 +1,109 @@
 from tkinter import *
-import backend
+from OO_backend import BooksBackend
 
-row = (-1,)
+class BooksFrontend:
 
-# This parameter has information about the event
-def get_selected_row(event):
-    global row
-    if len(lb.curselection()) > 0:
-        # This gives back a tuple, but we only want the first number (the row)
-        index = lb.curselection()[0]
-        row = lb.get(index)
+    row = (-1,)
 
-        # Clear the entry boxes and add the information from the thing you just clicked
-        clear_entries()
-        title_e.insert(END, row[1])
-        author_e.insert(END, row[2])
-        year_e.insert(END, row[3])
-        isbn_e.insert(END, row[4])
+    def __init__(self):
 
-def clear_entries():
-    title_e.delete(0, END)
-    author_e.delete(0, END)
-    year_e.delete(0, END)
-    isbn_e.delete(0, END)
+        self.gui = Tk()
 
-def view():
-    lb.delete(0, END)
-    for row in backend.view_all():
-        lb.insert(END, row)
+        self.gui.wm_title("Bookstore")
 
-# We need to use these wrappers because we're not allowed to use inputs in the command=? part
-def search():
-    lb.delete(0, END)
-    for row in backend.search(title_e.get(), author_e.get(), year_e.get(), isbn_e.get()):
-        lb.insert(END, row)
+        # Static labels
+        Label(self.gui, text="Title").grid(row=0, column=0)
+        Label(self.gui, text="Author").grid(row=0, column=2)
+        Label(self.gui, text="Year").grid(row=1, column=0)
+        Label(self.gui, text="ISBN").grid(row=1, column=2)
+
+        # Entry boxes
+        self.title_e = Entry(self.gui, width=15)
+        self.title_e.grid(row=0, column=1)
+
+        self.author_e = Entry(self.gui, width=15)
+        self.author_e.grid(row=0, column=3)
+
+        self.year_e = Entry(self.gui, width=15)
+        self.year_e.grid(row=1, column=1)
+
+        self.isbn_e = Entry(self.gui, width=15)
+        self.isbn_e.grid(row=1, column=3)
+
+    # This parameter has information about the event
+    def get_selected_row(event):
+        global row
+        if len(lb.curselection()) > 0:
+            # This gives back a tuple, but we only want the first number (the row)
+            index = lb.curselection()[0]
+            row = lb.get(index)
+
+            # Clear the entry boxes and add the information from the thing you just clicked
+            clear_entries()
+            title_e.insert(END, row[1])
+            author_e.insert(END, row[2])
+            year_e.insert(END, row[3])
+            isbn_e.insert(END, row[4])
+
+    def clear_entries():
+        title_e.delete(0, END)
+        author_e.delete(0, END)
+        year_e.delete(0, END)
+        isbn_e.delete(0, END)
+
+    def view():
+        lb.delete(0, END)
+        for row in backend.view_all():
+            lb.insert(END, row)
+
+    # We need to use these wrappers because we're not allowed to use inputs in the command=? part
+    def search():
+        lb.delete(0, END)
+        for row in backend.search(title_e.get(), author_e.get(), year_e.get(), isbn_e.get()):
+            lb.insert(END, row)
+        
+    def insert():
+        backend.insert(title_e.get(), author_e.get(), year_e.get(), isbn_e.get())
+        view()
+
+    def update():
+        if len(row) > 1:
+            backend.update(row[0], title_e.get(), author_e.get(), year_e.get(), isbn_e.get())
+            view()
+
+    def delete():
+        if len(row) > 1:
+            # The tuple you get from "row" in get_selected_row() starts with the ID
+            backend.delete(row[0])
+            view()
+            clear_entries()
+
+
+
     
-def insert():
-    backend.insert(title_e.get(), author_e.get(), year_e.get(), isbn_e.get())
-    view()
 
-def update():
-    if len(row) > 1:
-        backend.update(row[0], title_e.get(), author_e.get(), year_e.get(), isbn_e.get())
-        view()
+    # Listbox
+    lb = Listbox(gui, width=20)
+    lb.grid(row=2, column=0, rowspan=6, columnspan=2)
 
-def delete():
-    if len(row) > 1:
-        # The tuple you get from "row" in get_selected_row() starts with the ID
-        backend.delete(row[0])
-        view()
-        clear_entries()
+    # Scrollbar
+    sb = Scrollbar(gui)
+    sb.grid(row=2, column=2, rowspan=6)
 
-gui = Tk()
+    # Link listbox and scrollbar
+    lb.configure(yscrollcommand=sb.set)
+    sb.configure(command=lb.yview)
 
-gui.wm_title("Bookstore")
+    # This tells the Listbox to call get_selected_row when something is selected in it.
+    lb.bind("<<ListboxSelect>>", get_selected_row)
 
-# Static labels
-Label(gui, text="Title").grid(row=0, column=0)
-Label(gui, text="Author").grid(row=0, column=2)
-Label(gui, text="Year").grid(row=1, column=0)
-Label(gui, text="ISBN").grid(row=1, column=2)
+    # Buttons
+    buttonwidth = 10
+    Button(gui, text="View all", width=buttonwidth, command=view).grid(row=2, column=3)
+    Button(gui, text="Search entry", width=buttonwidth, command=search).grid(row=3, column=3)
+    Button(gui, text="Add entry", width=buttonwidth, command=insert).grid(row=4, column=3)
+    Button(gui, text="Update entry", width=buttonwidth, command=update).grid(row=5, column=3)
+    Button(gui, text="Delete entry", width=buttonwidth, command=delete).grid(row=6, column=3)
+    Button(gui, text="Close", width=buttonwidth, command=gui.destroy).grid(row=7, column=3)
 
-# Entry boxes
-title_e = Entry(gui, width=15)
-title_e.grid(row=0, column=1)
-
-author_e = Entry(gui, width=15)
-author_e.grid(row=0, column=3)
-
-year_e = Entry(gui, width=15)
-year_e.grid(row=1, column=1)
-
-isbn_e = Entry(gui, width=15)
-isbn_e.grid(row=1, column=3)
-
-# Listbox
-lb = Listbox(gui, width=20)
-lb.grid(row=2, column=0, rowspan=6, columnspan=2)
-
-# Scrollbar
-sb = Scrollbar(gui)
-sb.grid(row=2, column=2, rowspan=6)
-
-# Link listbox and scrollbar
-lb.configure(yscrollcommand=sb.set)
-sb.configure(command=lb.yview)
-
-# This tells the Listbox to call get_selected_row when something is selected in it.
-lb.bind("<<ListboxSelect>>", get_selected_row)
-
-# Buttons
-buttonwidth = 10
-Button(gui, text="View all", width=buttonwidth, command=view).grid(row=2, column=3)
-Button(gui, text="Search entry", width=buttonwidth, command=search).grid(row=3, column=3)
-Button(gui, text="Add entry", width=buttonwidth, command=insert).grid(row=4, column=3)
-Button(gui, text="Update entry", width=buttonwidth, command=update).grid(row=5, column=3)
-Button(gui, text="Delete entry", width=buttonwidth, command=delete).grid(row=6, column=3)
-Button(gui, text="Close", width=buttonwidth, command=gui.destroy).grid(row=7, column=3)
-
-gui.mainloop()
+    gui.mainloop()
