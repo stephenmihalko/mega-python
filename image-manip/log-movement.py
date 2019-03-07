42cbfa51895568386d4400e9336a5a9eb678e9fa
stephenmihalko <47727340+stephenmihalko@users.noreply.github.com>
2019-03-07 08:43:04 -0500

    Create log-movement.py

---
+++ image-manip/log-movement.py
@@ -0,0 +1,27 @@
+# A script to detect movement on the webcam and log the times at which movement was found.
+
+import cv2
+
+# Get the video stream from the webcam
+video_stream = cv2.VideoCapture(0)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# MAKE SURE YOU RELEASE THE STREAM
+video_stream.release()
